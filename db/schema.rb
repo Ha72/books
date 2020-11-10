94f69386b5544af007afe8c3ef2a9c1c44195075
Ha72 <phuonglh72@gmail.com>
2020-11-09 18:18:23 -0600

    Added Province model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_001516) do
+ActiveRecord::Schema.define(version: 2020_11_10_001745) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2020_11_10_001516) do
 
   create_table "genres", force: :cascade do |t|
     t.string "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.decimal "gst"
+    t.decimal "pst"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
