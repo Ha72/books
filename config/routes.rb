415f59961687c204d1aa9fb0550d6ccf68bf26ac
Ha72 <phuonglh72@gmail.com>
2020-11-10 16:31:55 -0600

    Generated Book controller and Book index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: "books#index"
+  
+  resources :books, only: %i[index show]
+  
 end
