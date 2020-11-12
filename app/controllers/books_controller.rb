9e8901de2960c7203144c66d4467c7d5b9d1506c
Ha72 <phuonglh72@gmail.com>
2020-11-11 19:36:41 -0600

    Updated filter and search conditions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   end
   
   def new
-    @new_books = Book.where(status: "new")
+    @new_books = Book.where("created_at >= ?", Time.new - 3*24*60*60)
                     .page(params[:page]).per(3)
   end
   
@@ -18,7 +18,8 @@ class BooksController < ApplicationController
   end
   
   def recently_updated
-    @updated_books = Book.where(status: "recently updated")
+    @updated_books = Book.where("updated_at >= ?", Time.new - 3*24*60*60)
+                        .where("created_at < ?", Time.new - 3*24*60*60)
                         .page(params[:page]).per(3)
   end
   
@@ -26,12 +27,11 @@ class BooksController < ApplicationController
       keyword_search = "%#{params[:keyword]}%"
       @genre_search = "#{params[:genre]}"
       
-      if @genre_search == "All Genre"
-        @search_books = Book.where("title LIKE ?", keyword_search)
-      else
+      @search_books = Book.where("title LIKE ? OR description LIKE ?", keyword_search, keyword_search)
+      
+      if @genre_search != "All Genre"
         genre_id = Genre.find_by(genre: @genre_search)
-        @search_books = Book.where("title LIKE ?", keyword_search)
-                            .where(genre_id: genre_id)
+        @search_books = @search_books.where(genre_id: genre_id)
       end
       
       @paginated_books = @search_books.page(params[:page]).per(3)
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
     has_many :order_books
     has_many :orders, through: :order_books
     
-    validates :title, :author, :price, :stock_quantity, :status, presence: true
+    validates :title, :author, :price, :stock_quantity, presence: true
     validates :stock_quantity, numericality: true
     
     has_one_attached :image
